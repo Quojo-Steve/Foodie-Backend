4de48a86fbc12eaa41cf8ddc79588a04f5fb6eaa
Quojo-Steve <quojosteve@gmail.com>
2025-07-18 15:04:58 +0000

    Made the api publicly viewable

--- app/models.py
+++ app/models.py
@@ -6,6 +6,11 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
     is_verified = db.Column(db.Boolean, default=False)
+    phone_number = db.Column(db.String(15), nullable=False)
     otp_code = db.Column(db.String(6), nullable=True)
     otp_expiry = db.Column(db.DateTime, nullable=True)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+# class UserProfile(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     # user_id = db.Column(db.Integer, )
